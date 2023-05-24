375724624bd662487d9cf3ac245b659857f08f41
huan572001 <85747713+huan572001@users.noreply.github.com>
2023-05-24 13:56:45 +0700

    add list status umbrella

--- assets/script/UmbrellaController.ts
+++ assets/script/UmbrellaController.ts
@@ -1,4 +1,13 @@
-import { _decorator, Component, Node } from 'cc';
+import {
+  _decorator,
+  Component,
+  ImageAsset,
+  Node,
+  resources,
+  Sprite,
+  SpriteFrame,
+  Texture2D,
+} from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('UmbrellaController')
@@ -7,11 +16,19 @@ export class UmbrellaController extends Component {
   private _flagged: boolean = false;
   private _bombExist: boolean = false;
   private _number: number = 0;
+  @property({ type: Sprite })
+  private spriteUmbrella: Sprite;
+  @property({ type: [SpriteFrame] })
+  private listStatusUmbrella: SpriteFrame[] = [];
+
   start() {
     this.node.on(Node.EventType.TOUCH_START, this.openUmbrella, this);
   }
 
-  public openUmbrella(): void {}
+  public openUmbrella(): void {
+    if (this._number > 0)
+      this.spriteUmbrella.spriteFrame = this.listStatusUmbrella[this._number];
+  }
   update(deltaTime: number) {}
   public get bombExist(): boolean {
     return this._bombExist;
